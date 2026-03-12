18758ac466fded30f3d222e5bcfa4c5414261cdd
SeungWoo1222 <wlstmddn98@naver.com>
2026-03-12 17:00:25 +0900

    [FEAT] redis ci 테스트

--- src/test/java/com/toyboyz/fileconversion/FileConversionApplicationTests.java
+++ src/test/java/com/toyboyz/fileconversion/FileConversionApplicationTests.java
@@ -2,8 +2,10 @@ package com.toyboyz.fileconversion;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class FileConversionApplicationTests {
 
     @Test
