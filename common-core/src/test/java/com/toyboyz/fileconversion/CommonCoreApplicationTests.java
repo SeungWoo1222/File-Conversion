79c20f3f6963184bcfc8d50aabebea35bfb38642
SeungWoo1222 <wlstmddn98@naver.com>
2026-04-09 14:41:51 +0900

    test 어노테이션 제거

--- common-core/src/test/java/com/toyboyz/fileconversion/CommonCoreApplicationTests.java
+++ common-core/src/test/java/com/toyboyz/fileconversion/CommonCoreApplicationTests.java
@@ -3,7 +3,6 @@ package com.toyboyz.fileconversion;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class CommonCoreApplicationTests {
 
     @Test
