f969283257967a16636d8e9c66e1b32fcfb47f35
SeungWoo1222 <wlstmddn98@naver.com>
2026-04-09 16:10:14 +0900

    cd 테스트2

--- api-app/src/redisIntegrationTest/java/com/toyboyz/fileconversion/redis/RedisConnectionIntegrationTest.java
+++ api-app/src/redisIntegrationTest/java/com/toyboyz/fileconversion/redis/RedisConnectionIntegrationTest.java
@@ -1,6 +1,5 @@
 package com.toyboyz.fileconversion.redis;
 
-import com.redis.testcontainers.RedisContainer;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
