1f5c212aeb0b65ce64add2c98c18cd9d4f19417f
SeungWoo1222 <wlstmddn98@naver.com>
2026-04-10 16:14:47 +0900

    S3Config region 맵핑

--- infra-s3/src/main/java/com/toyboyz/fileconversion/infra/s3/config/S3Config.java
+++ infra-s3/src/main/java/com/toyboyz/fileconversion/infra/s3/config/S3Config.java
@@ -35,7 +35,7 @@ public class S3Config {
 // }
 
     @Bean
-    public S3Client s3Client(@Value("${app.s3.region}") String region) {
+    public S3Client s3Client(@Value("${s3.region}") String region) {
         return S3Client.builder()
                 .region(Region.of(region))
                 .credentialsProvider(DefaultCredentialsProvider.create())
@@ -44,7 +44,7 @@ public class S3Config {
 
 
     @Bean
-    public S3Presigner s3Presigner(@Value("${app.s3.region}") String region) {
+    public S3Presigner s3Presigner(@Value("${s3.region}") String region) {
         return S3Presigner.builder()
                 .region(Region.of(region))
                 .credentialsProvider(DefaultCredentialsProvider.create())
