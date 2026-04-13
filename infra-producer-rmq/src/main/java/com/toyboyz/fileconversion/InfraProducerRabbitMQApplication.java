3e0fc8745304e1b3b7a37772706c1f6cccb8171e
Jwwone <bau12288@gmail.com>
2026-04-13 22:00:33 +0900

    [chore]: monituring

--- infra-producer-rmq/src/main/java/com/toyboyz/fileconversion/InfraProducerRabbitMQApplication.java
+++ infra-producer-rmq/src/main/java/com/toyboyz/fileconversion/InfraProducerRabbitMQApplication.java
@@ -6,6 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class InfraProducerRabbitMQApplication {
         public static void main(String[] args) {
-            SpringApplication.run(InfraProducerRabbitMQApplication.class, args);
+                SpringApplication.run(InfraProducerRabbitMQApplication.class, args);
     }
 }
--- worker-app/src/main/java/com/toyboyz/fileconversion/WorkerAppApplication.java
+++ worker-app/src/main/java/com/toyboyz/fileconversion/WorkerAppApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WorkerAppApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(WorkerAppApplication.class, args);
     }
-
 }
