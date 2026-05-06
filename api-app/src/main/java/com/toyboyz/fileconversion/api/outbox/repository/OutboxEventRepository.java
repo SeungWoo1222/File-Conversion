c29bfce699b721cc9a11cc5ec0b27ad9969ddb40
Bemay <121229068+SeungWoo1222@users.noreply.github.com>
2026-05-06 18:33:13 +0900

    Update OutboxEventRepository.java

--- api-app/src/main/java/com/toyboyz/fileconversion/api/outbox/repository/OutboxEventRepository.java
+++ api-app/src/main/java/com/toyboyz/fileconversion/api/outbox/repository/OutboxEventRepository.java
@@ -4,5 +4,5 @@ package com.toyboyz.fileconversion.api.outbox.repository;
 import com.toyboyz.fileconversion.api.outbox.entity.OutboxEvent;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OutboxEventRepository extends JpaRepository<OutboxEvent, String> {
+public interface OutboxEventRepository extends JpaRepository<OutboxEvent, Long> {
 }
